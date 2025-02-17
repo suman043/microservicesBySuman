d6303805f35c0fa07930899a7fe751f8833153e2
suman043 <suman.mohanty043@gmail.com>
2025-02-17 17:36:49 +0530

    default basic empty filter in api gateway

---
+++ 06_API_Gateway/src/main/java/com/example/filer/MyFilter.java
@@ -0,0 +1,21 @@
+package com.example.filer;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class MyFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        System.out.println(" Filter() - executed....  ");
+
+        //Write Validation Request Code below
+
+        return chain.filter(exchange);
+    }
+}
