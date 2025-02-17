28564c09c7274d9aff7f8b7a7253d86a9e640a1a
suman043 <suman.mohanty043@gmail.com>
2025-02-17 16:04:02 +0530

    Load balancing

--- 04_Welcome_Service/src/main/java/com/example/controller/WelcomeRestController.java
+++ 04_Welcome_Service/src/main/java/com/example/controller/WelcomeRestController.java
@@ -1,13 +1,21 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeRestController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/welcome")
     public String getWelcomeMsg(){
-        return "Welcome";
+
+        String port = environment.getProperty("server.port");
+
+        return "Welcome"+" ("+port+")";
     }
 }
