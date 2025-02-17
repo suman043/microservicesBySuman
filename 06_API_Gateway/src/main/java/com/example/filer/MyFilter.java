01248c57e7bf1bc3b446ea901a77f90d46e9f871
suman043 <suman.mohanty043@gmail.com>
2025-02-17 18:18:16 +0530

    Implementing hardcoded filter in api gateway

--- 06_API_Gateway/src/main/java/com/example/filer/MyFilter.java
+++ 06_API_Gateway/src/main/java/com/example/filer/MyFilter.java
@@ -2,9 +2,14 @@ package com.example.filer;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Set;
 
 @Component
 public class MyFilter implements GlobalFilter {
@@ -15,6 +20,19 @@ public class MyFilter implements GlobalFilter {
         System.out.println(" Filter() - executed....  ");
 
         //Write Validation Request Code below
+
+        ServerHttpRequest request = exchange.getRequest();
+        HttpHeaders headers = request.getHeaders();
+        Set<String> keySet = headers.keySet();
+
+        if(!keySet.contains("Secret")){
+            throw new RuntimeException("Invalid Request");
+        }
+
+        List<String> list = headers.get("Secret");
+        if(!list.get(0).equals("suman@123")){
+            throw new RuntimeException("Invalid Request");
+        }
 
         return chain.filter(exchange);
     }
